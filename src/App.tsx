0ca7f2763d9271065327fac6c5ce0b5089c9b404
joejowjow <yokyheikal@hotmail.com>
2024-10-21 23:27:02 +0300

    updat 2

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,11 @@ const projects = [
     description: 'Immersive CGI animation creating a breathtaking fantasy world for a game trailer',
     videoUrl: 'https://drive.google.com/drive/folders/1_1dNxrttYBPCj05MmB7ZZ7HmT9uXjU6r?usp=drive_link',
   },
+  {
+    title: 'Real Estate Renders',
+    description: 'Immersive CGI animation creating a breathtaking fantasy world for a game trailer',
+    videoUrl: 'https://drive.google.com/drive/u/0/folders/1NuVI-Rcu4HYKbu-vDQeCZD3QOPcwZTF2',
+  },
 ];
 
 const freelancerProfileUrl = 'https://www.freelancer.com/u/youssef506';
